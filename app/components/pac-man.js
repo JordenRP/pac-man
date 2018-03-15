ae4e2d3963589c9ba25c2780334b823ab1cf1085
Pavel <gritsatsuev.pavel@gmail.com>
2018-03-15 05:35:29 +0400

    Animations

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -2,16 +2,18 @@ import Ember from 'ember';
 import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/component';
 
 export default Ember.Component.extend(KeyboardShortcuts, {
-  didInsertElement: function () {
+  didInsertElement() {
     this.drawPac();
     this.drawGrid();
   },
   
+  levelNumber: 1,
   score: 0,
-
   x: 1,
   y: 2,
   squareSize: 40,
+  isMoving: false,
+  direction: 'stopped',
 
   ctx: Ember.computed(function () {
     let canvas = document.getElementById("myCanvas");
@@ -35,12 +37,12 @@ export default Ember.Component.extend(KeyboardShortcuts, {
     return this.get('screenHeight') * this.get('squareSize');
   }),
 
-  clearScreen: function () {
+  clearScreen() {
     let ctx = this.get('ctx');
     ctx.clearRect(0, 0, this.get('screenPixelWidth'), this.get('screenPixelHeight'));
   },
   
-  processAnyPellets: function(){
+  processAnyPellets(){
   let x = this.get('x');
   let y = this.get('y');
   let grid = this.get('grid');
@@ -48,37 +50,62 @@ export default Ember.Component.extend(KeyboardShortcuts, {
   if(grid[y][x] == 2){
     grid[y][x] = 0;
     this.incrementProperty('score')
-  }
+    if(this.levelComplete()){
+      this.incrementProperty('levelNumber')
+      this.restartLevel()
+    }
+  } 
 },
 
-movePacMan(direction, amount){
-  this.incrementProperty(direction, amount);
-
-  if(this.collidedWithBorder() || this.collidedWithWall()) {
-    this.decrementProperty(direction, amount)
+movePacMan(direction) {
+  let inputBlocked = this.get('isMoving') || this.pathBlockedInDirection(direction)
+  if(!inputBlocked){
+    this.set('direction', direction)
+    this.set('isMoving', true)
+    this.movementLoop()
   }
+},
+  
+frameCycle: 1,
+framesPerMovement: 30,
+movementLoop(){
+  if(this.get('frameCycle') == this.get('framesPerMovement')){
+    let direction = this.get('direction')
+    this.set('x', this.nextCoordinate('x', direction));
+    this.set('y', this.nextCoordinate('y', direction));
 
-  this.processAnyPellets()
+    this.set('isMoving', false);
+    this.set('frameCycle', 1);
+
+    this.processAnyPellets();
+  } else {
+    this.incrementProperty('frameCycle');
+    Ember.run.later(this, this.movementLoop, 1000/60);
+  }
 
   this.clearScreen();
   this.drawGrid();
   this.drawPac();
 },
-
-  keyboardShortcuts: {
-    up: function () {
-      this.movePacMan('y', -1);
-    },
-    down: function () {
-      this.movePacMan('y', 1);
-    },
-    left: function () {
-      this.movePacMan('x', -1);
-    },
-    right: function () {
-      this.movePacMan('x', 1);
-    },
-  },
+  
+nextCoordinate(coordinate, direction){
+  return this.get(coordinate) + this.get(`directions.${direction}.${coordinate}`);
+},
+  
+directions: {
+  'up': {x: 0, y: -1},
+  'down': {x: 0, y: 1},
+  'left': {x: -1, y: 0},
+  'right': {x: 1, y: 0},
+  'stopped': {x: 0, y: 0}
+},
+  
+keyboardShortcuts: {
+  up() { this.movePacMan('up');},
+  down()  { this.movePacMan('down');},
+  left() { this.movePacMan('left');},
+  right() { this.movePacMan('right');},
+},
 
 grid: [
   [2, 2, 2, 2, 2, 2, 2, 1],
@@ -89,7 +116,7 @@ grid: [
   [1, 2, 2, 2, 2, 2, 2, 1],
 ],
   
-drawWall: function(x, y){
+drawWall(x, y){
   let ctx = this.get('ctx');
   let squareSize = this.get('squareSize');
 
@@ -104,20 +131,21 @@ drawPac(){
   let x = this.get('x');
   let y = this.get('y');
   let radiusDivisor = 2;
-  this.drawCircle(x, y, radiusDivisor)
+  this.drawCircle(x, y, radiusDivisor, this.get('direction'));
 },
 
 drawPellet(x, y){
   let radiusDivisor = 6;
-  this.drawCircle(x, y, radiusDivisor)
+  this.drawCircle(x, y, radiusDivisor, 'stopped');
 },
 
-drawCircle(x, y, radiusDivisor) {
+
+drawCircle(x, y, radiusDivisor, direction) {
   let ctx = this.get('ctx')
   let squareSize = this.get('squareSize');
 
-  let pixelX = (x+1/2) * squareSize;
-  let pixelY = (y+1/2) * squareSize;
+  let pixelX = (x + 1/2 + this.offsetFor('x', direction)) * squareSize;
+  let pixelY = (y + 1/2 + this.offsetFor('y', direction)) * squareSize;
 
   ctx.fillStyle = '#000';
   ctx.beginPath();
@@ -126,7 +154,12 @@ drawCircle(x, y, radiusDivisor) {
   ctx.fill();
 },
 
-drawGrid: function(){
+offsetFor(coordinate, direction){
+  let frameRatio = this.get('frameCycle') / this.get('framesPerMovement');
+  return this.get(`directions.${direction}.${coordinate}`) * frameRatio;
+},
+
+drawGrid(){
   let grid = this.get('grid');
   grid.forEach((row, rowIndex)=>{
     row.forEach((cell, columnIndex)=>{
@@ -140,25 +173,45 @@ drawGrid: function(){
   })
 },
   
-  collidedWithWall: function(){
-    let x = this.get('x');
-    let y = this.get('y');
-    let grid = this.get('grid');
-
-    return grid[y][x] == 1
+ pathBlockedInDirection(direction) {
+  let cellTypeInDirection = this.cellTypeInDirection(direction);
+  return Ember.isEmpty(cellTypeInDirection) || cellTypeInDirection === 1;
 },
 
-  collidedWithBorder: function () {
-    let x = this.get('x');
-    let y = this.get('y');
-    let screenHeight = this.get('screenHeight');
-    let screenWidth = this.get('screenWidth');
+cellTypeInDirection(direction) {
+  let nextX = this.nextCoordinate('x', direction);
+  let nextY = this.nextCoordinate('y', direction);
 
+  return this.get(`grid.${nextY}.${nextX}`);
+},
+  
+levelComplete(){
+    let hasPelletsLeft = false;
+    let grid = this.get('grid');
 
-    let pacOutOfBounds = x < 0 ||
-                         y < 0 ||
-                         x >= screenWidth ||
-                         y >= screenHeight
-    return pacOutOfBounds
-  }
+    grid.forEach((row)=>{
+      row.forEach((cell)=>{
+        if(cell == 2){
+          hasPelletsLeft = true
+        }
+      })
+    })
+    return !hasPelletsLeft;
+},
+  
+  
+restartLevel(){
+  this.set('x', 1);
+  this.set('y', 2);
+
+  let grid = this.get('grid');
+  grid.forEach((row, rowIndex)=>{
+    row.forEach((cell, columnIndex)=>{
+      if(cell == 0){
+        grid[rowIndex][columnIndex] = 2
+      }
+    })
+  })
+},
+  
 });
